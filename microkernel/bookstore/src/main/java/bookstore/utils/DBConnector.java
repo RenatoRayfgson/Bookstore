07b82bb3ce2fa1804cb644891f45a8c026797787
Renato Rayfgson <rrayfgson@gmail.com>
2025-07-14 15:39:03 -0300

    Inicio do backend

--- microkernel/bookstore/src/main/java/bookstore/utils/DBConnector.java
+++ microkernel/bookstore/src/main/java/bookstore/utils/DBConnector.java
@@ -1,5 +1,33 @@
 package bookstore.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnector {
 
+    private static final String JDBC_URL = "jdbc:mariadb://127.0.0.1:3307/bookstore";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() throws SQLException {
+        // Garante que o driver JDBC é carregado
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("MariaDB JDBC Driver not found: " + e.getMessage());
+            throw new SQLException("MariaDB JDBC Driver not found.", e);
+        }
+        return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.err.println("Error closing connection: " + e.getMessage());
+            }
+        }
+    }
 }
--- microkernel/bookstore/src/main/java/bookstore/utils/PersonManager.java
+++ microkernel/bookstore/src/main/java/bookstore/utils/PersonManager.java
@@ -1,5 +1,34 @@
 package bookstore.utils;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import bookstore.models.Person;
+
 public class PersonManager {
 
+    public Person addPerson(Person client){
+        String sql = "INSERT INTO person (name, email) VALUES (?, ?)";
+        try(Connection connection = DBConnector.getConnection();
+            PreparedStatement pstmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+            pstmt.setString(1, client.getName());
+            pstmt.setString(2, client.getEmail());
+            int affectedRows = pstmt.executeUpdate();
+            if(affectedRows > 0){
+                try(ResultSet rs = pstmt.getGeneratedKeys()){
+                    if(rs.next()){
+                        client.setPersonId(rs.getInt(1));
+                    }
+                }
+            }
+            return client;
+        } catch (SQLException e) {
+            System.err.println("Error while trying to add the new client: " + e.getMessage());
+            return null;
+        }
+
+    }
 }
