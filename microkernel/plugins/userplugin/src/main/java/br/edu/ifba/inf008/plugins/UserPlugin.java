6b8c5515e6302a36537e69df09d60fce6dfa7c4c
Renato Rayfgson <rrayfgson@gmail.com>
2025-07-22 01:42:09 -0300

    plugins stt

---
+++ microkernel/plugins/userplugin/src/main/java/br/edu/ifba/inf008/plugins/UserPlugin.java
@@ -0,0 +1,26 @@
+package br.edu.ifba.inf008.plugins;
+
+import br.edu.ifba.inf008.interfaces.ICore;
+import br.edu.ifba.inf008.interfaces.IPlugin;
+import br.edu.ifba.inf008.interfaces.IUIController;
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class UserPlugin implements IPlugin
+{
+    @Override //Adicionado depois
+    public boolean init() {
+        IUIController uiController = ICore.getInstance().getUIController();
+
+        MenuItem menuItem = uiController.createMenuItem("Menu 2", "My Menu Item");
+        menuItem.setOnAction((ActionEvent e) -> { //Linha antiga: menuItem.setOnAction(new EventHandler<ActionEvent>()
+            System.out.println("I've been clicked!");
+        });
+
+        uiController.createTab("new tab 2", new Rectangle(200,200, Color.LIGHTSTEELBLUE));
+
+        return true;
+    }
+}
